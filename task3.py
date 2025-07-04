7e28e2b78a657d39bf2bd60f79eda15966252ab4
SuvarnaBandaru <158791558+suvarnabandaru80@users.noreply.github.com>
2025-07-04 20:27:33 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,69 @@
+#Basic Input with Integer Conversion Take two numbers as input from the user, convert them to integers, and print their sum.
+# num1=input("Enter the number 1:")
+# num2=input("Enter the num2:")
+# num3=int(num1)
+# num4=int(num2)
+# res=num3+num4
+# print(res)
+
+#Multiple Inputs with Space Separation Take two floating-point numbers as input in a single line separated by space and print their product.
+# num1,num2=input("ENter the float:").split()
+# num1=float(num1)
+# num2=float(num2)
+# s3=num1*num2
+# print(s3)
+
+# Expression Evaluation with eval() Take a mathematical expression from the user as input and print the evaluated result
+# expression=input("enter mathematical expression:")
+# result=eval(expression)
+# print(result)
+
+#List Input using eval() Take a list as input using eval() and print the sum of its elements.
+# list=input("Enter the input:")
+# result=eval(list)
+# results=sum(result)
+# print(results)
+
+# Boolean Type Conversion Take an input from the user, convert it to boolean, and print the result.
+# b1=input("Enter the input:")
+# b2=input("Enter the input:")
+# b3=bool(b1)
+# b4=bool(b2)
+# print(b3)
+# print(b4)
+
+
+#String to Float Conversion Take a floating-point number as string input, convert it to float, and print its square.
+# num1=input("Enter the number1:")
+# num2=input("Enter the number2:")
+# num1=float(num1)
+# num2=float(num2)
+# square=num1*num1
+# square1=num2*num2
+# square3=square,square1
+# print(square3)
+
+#Dictionary Input using eval() Take a dictionary as input using eval() and print its keys and values separately.
+# dict=input("Enter the dictionary:")
+# res=eval(dict)
+# print(res)
+
+#List of Integers from Space Separated Input Take a list of integers as input in a single line separated by space and print the maximum and minimum values.
+# h=input("Enter the input:").split()
+# h1 = max(h)
+# h2 = min(h)
+# print(h1)
+# print(h2)
+
+#Mixed Type Literal Input with eval()Take any Python literal as input (string, list, tuple, int, etc.) using eval() and print its type.
+# h=input("Enter the input")
+# res=eval(h)
+# print(type(res))
+
+#Combined Type Conversion Take two inputs, convert the first to integer and second to float, then print their sum.
+# h1=input("Enter the input:")
+# h2=input("Enter the input2:")
+# h3=int(h1)
+# h4=float(h2)
+# h5=h3+h4
+# print(h5)
